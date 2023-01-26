c2936fad3d55c7690de53d8935f3dac87a3a9b16
Albert <kibernetik.b02@mail.ru>
2023-01-26 20:17:33 +0300

    create model 'directory'

--- terminology_service/app/models.py
+++ terminology_service/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Directory(models.Model):
+    id = models.AutoField(primary_key=True)
+    code = models.CharField('Код', max_length=100, blank=False, unique=True)
+    name = models.CharField('Наименование', max_length=300, blank=False)
+    description = models.TextField('Описание', blank=True)
+
+    class Meta:
+        verbose_name = 'Справочник'
+        verbose_name_plural = 'Справочники'
+        ordering = ['name']
+
+    def __str__(self) -> str:
+        return f'{self.name}'
