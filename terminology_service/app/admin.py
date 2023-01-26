6a81fb9d080948e027cb857797fd1edfe121a36f
Albert <kibernetik.b02@mail.ru>
2023-01-26 22:45:24 +0300

    create model 'directory_version'

--- terminology_service/app/admin.py
+++ terminology_service/app/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Directory
+from .models import (
+    Directory,
+    DirectoryVersion
+)
 
 # Register your models here.
 
 
 
 admin.site.register(Directory)
+admin.site.register(DirectoryVersion)
--- terminology_service/app/models.py
+++ terminology_service/app/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 
 class Directory(models.Model):
-    id = models.AutoField(primary_key=True)
     code = models.CharField('Код', max_length=100, blank=False, unique=True)
     name = models.CharField('Наименование', max_length=300, blank=False)
     description = models.TextField('Описание', blank=True)
@@ -16,3 +15,18 @@ class Directory(models.Model):
 
     def __str__(self) -> str:
         return f'{self.name}'
+
+
+class DirectoryVersion(models.Model):
+    directory = models.ForeignKey(Directory, models.PROTECT, blank=False, verbose_name='Справочник')
+    version = models.CharField('Версия', max_length=50, blank=False)
+    start_date = models.DateField('Дата начала действия', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Версия справочника'
+        verbose_name_plural = 'Версии справочников'
+        ordering = ['directory', 'version']
+        unique_together = (('directory', 'version'), ('directory', 'start_date'))
+        
+    def __str__(self) -> str:
+        return f'{self.id_directory.name} v{self.version}'
